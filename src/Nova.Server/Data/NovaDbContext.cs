6179670c4b27053e3802e727b26c82768b9aa581
asherpereira <pereiraasher0@gmail.com>
2026-09-25 15:29:41 +1000

    feat: expand EF domain model

--- src/Nova.Server/Data/NovaDbContext.cs
+++ src/Nova.Server/Data/NovaDbContext.cs
@@ -7,6 +7,14 @@ public sealed class NovaDbContext(DbContextOptions<NovaDbContext> options) : DbC
 {
     public DbSet<User> Users => Set<User>();
     public DbSet<EncryptedMessage> Messages => Set<EncryptedMessage>();
+    public DbSet<Conversation> Conversations => Set<Conversation>();
+    public DbSet<ConversationMember> ConversationMembers => Set<ConversationMember>();
+    public DbSet<NovaServer> Servers => Set<NovaServer>();
+    public DbSet<ServerMember> ServerMembers => Set<ServerMember>();
+    public DbSet<Role> Roles => Set<Role>();
+    public DbSet<Channel> Channels => Set<Channel>();
+    public DbSet<Device> Devices => Set<Device>();
+    public DbSet<RefreshSession> RefreshSessions => Set<RefreshSession>();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -25,6 +33,91 @@ public sealed class NovaDbContext(DbContextOptions<NovaDbContext> options) : DbC
             entity.HasIndex(x => new { x.ConversationId, x.CreatedAt });
             entity.Property(x => x.Ciphertext).IsRequired();
             entity.Property(x => x.Nonce).HasMaxLength(256).IsRequired();
+            entity.Property(x => x.EncryptionVersion).IsRequired();
+        });
+
+        modelBuilder.Entity<Conversation>(entity =>
+        {
+            entity.HasKey(x => x.Id);
+            entity.HasIndex(x => x.UpdatedAt);
+            entity.Property(x => x.Name).HasMaxLength(100);
+            entity.Property(x => x.IconUrl).HasMaxLength(2048);
+        });
+
+        modelBuilder.Entity<ConversationMember>(entity =>
+        {
+            entity.HasKey(x => new { x.ConversationId, x.UserId });
+            entity.HasIndex(x => new { x.UserId, x.ConversationId });
+            entity.HasOne(x => x.Conversation).WithMany(x => x.Members)
+                .HasForeignKey(x => x.ConversationId).OnDelete(DeleteBehavior.Cascade);
+            entity.HasOne(x => x.User).WithMany()
+                .HasForeignKey(x => x.UserId).OnDelete(DeleteBehavior.Cascade);
+        });
+
+        modelBuilder.Entity<NovaServer>(entity =>
+        {
+            entity.HasKey(x => x.Id);
+            entity.HasIndex(x => x.OwnerId);
+            entity.Property(x => x.Name).HasMaxLength(100).IsRequired();
+            entity.Property(x => x.Description).HasMaxLength(1000);
+            entity.Property(x => x.IconUrl).HasMaxLength(2048);
+        });
+
+        modelBuilder.Entity<ServerMember>(entity =>
+        {
+            entity.HasKey(x => new { x.ServerId, x.UserId });
+            entity.HasIndex(x => x.UserId);
+            entity.HasOne(x => x.Server).WithMany(x => x.Members)
+                .HasForeignKey(x => x.ServerId).OnDelete(DeleteBehavior.Cascade);
+            entity.HasOne(x => x.User).WithMany()
+                .HasForeignKey(x => x.UserId).OnDelete(DeleteBehavior.Cascade);
+        });
+
+        modelBuilder.Entity<Role>(entity =>
+        {
+            entity.HasKey(x => x.Id);
+            entity.HasIndex(x => new { x.ServerId, x.Position });
+            entity.Property(x => x.Name).HasMaxLength(100).IsRequired();
+            entity.HasOne(x => x.Server).WithMany(x => x.Roles)
+                .HasForeignKey(x => x.ServerId).OnDelete(DeleteBehavior.Cascade);
+        });
+
+        modelBuilder.Entity<Channel>(entity =>
+        {
+            entity.HasKey(x => x.Id);
+            entity.HasIndex(x => new { x.ServerId, x.Position });
+            entity.Property(x => x.Name).HasMaxLength(100).IsRequired();
+            entity.Property(x => x.Topic).HasMaxLength(1024);
+            entity.HasOne(x => x.Server).WithMany(x => x.Channels)
+                .HasForeignKey(x => x.ServerId).OnDelete(DeleteBehavior.Cascade);
+            entity.HasOne<Channel>().WithMany()
+                .HasForeignKey(x => x.ParentChannelId).OnDelete(DeleteBehavior.Restrict);
+        });
+
+        modelBuilder.Entity<Device>(entity =>
+        {
+            entity.HasKey(x => x.Id);
+            entity.HasIndex(x => new { x.UserId, x.RevokedAt });
+            entity.Property(x => x.Name).HasMaxLength(100).IsRequired();
+            entity.Property(x => x.Platform).HasMaxLength(32).IsRequired();
+            entity.Property(x => x.PushToken).HasMaxLength(4096);
+            entity.Property(x => x.IdentityPublicKey).HasMaxLength(8192);
+            entity.Property(x => x.SignedPreKey).HasMaxLength(8192);
+            entity.Property(x => x.PreKeySignature).HasMaxLength(8192);
+            entity.HasOne(x => x.User).WithMany()
+                .HasForeignKey(x => x.UserId).OnDelete(DeleteBehavior.Cascade);
+        });
+
+        modelBuilder.Entity<RefreshSession>(entity =>
+        {
+            entity.HasKey(x => x.Id);
+            entity.HasIndex(x => x.TokenHash).IsUnique();
+            entity.HasIndex(x => new { x.UserId, x.RevokedAt, x.ExpiresAt });
+            entity.Property(x => x.TokenHash).HasMaxLength(128).IsRequired();
+            entity.HasOne(x => x.User).WithMany()
+                .HasForeignKey(x => x.UserId).OnDelete(DeleteBehavior.Cascade);
+            entity.HasOne(x => x.Device).WithMany()
+                .HasForeignKey(x => x.DeviceId).OnDelete(DeleteBehavior.Cascade);
         });
     }
 }
